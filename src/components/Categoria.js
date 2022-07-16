422e2cc5b65246d534b2448bf93f9296e7d0c0be
MadarasiD <madarka0913@gmail.com>
2022-07-16 23:38:51 +0200

    update

--- src/components/Categoria.js
+++ src/components/Categoria.js
@@ -9,7 +9,18 @@ const Categoria = () => {
             <li className='active ctg-btn'><a href="/" style={{display: 'flex',alignItems: 'center', textAlign: 'center'}}><GiHamburgerMenu className='hambika' style={{marginRight: 5}} />Kategória</a>
             <div className='sub-menu-1'>
                 <ul>
-                  <li><a href="/">Tv és szórakozás</a></li>
+                  <li><a href="/">Tv és szórakozás</a>
+                  <div className="sub-menu-2" style={{position: 'absolute', marginLeft: 145, marginTop: -32}}>
+                    <ul>
+                    <li><a href="/">Telefon,Tablet,Laptop</a></li>
+                  <li><a href="/">Számítástechnika</a></li>
+                  <li><a href="/">Fotó és Videó</a></li>
+                  <li><a href="/">Okos Otthon</a></li>
+                  <li><a href="/">Kiegészítők</a></li>
+                  <li><a href="/">Könyvek</a></li>
+                    </ul>
+                  </div>
+                  </li>
                   <li><a href="/">Telefon,Tablet,Laptop</a></li>
                   <li><a href="/">Számítástechnika</a></li>
                   <li><a href="/">Fotó és Videó</a></li>
